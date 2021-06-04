db4f1b2d59a5bae4ec5ba1f7ccbd36f95585694b
Artemiy <69850801+kuzinartemiy@users.noreply.github.com>
2021-06-04 14:11:30 +0500

    feat: добавить отображение процентов с момента загрузки страницы

--- scripts/components/Coin.js
+++ scripts/components/Coin.js
@@ -2,6 +2,7 @@ export class Coin {
   constructor(coinData, coinLocalData, placeTemplate, updatePriceHandler, deleteCoinHandler) {
     this._updateInterval = 2000;
     this._coinDataToken = coinData.symbol;
+    this._startPrice = coinData.price;
     this._price = coinData.price;
     this._token = coinLocalData.token;
     this._coinCurrency = coinLocalData.currency;
@@ -11,6 +12,7 @@ export class Coin {
     this._coinElement = this._getTemplate();
     this._coinTitleElement = this._coinElement.querySelector('.coin__title');
     this._coinPriceElement = this._coinElement.querySelector('.coin__price');
+    this._coinChangeElement = this._coinElement.querySelector('.coin__change');
     this._deleteButtonElement = this._coinElement.querySelector('.coin__delete-button');
   }
 
@@ -25,11 +27,12 @@ export class Coin {
 
   generateCoin() {
     this._setEventListeners();
-    this._coinIconUrl = `https://cryptoicons.org/api/icon/${this._token.toLowerCase()}/200`;
+    this._coinIconUrl = `https://kuzinartemiy.github.io/petcoin-icons/icon/${this._token.toLowerCase()}.svg`;
     this._coinElement.querySelector('.coin__title').textContent = this._coinDataToken;
     this._coinElement.querySelector('.coin__price').textContent = this._price;
     this._coinElement.querySelector('.coin__icon').style.backgroundImage = `url(${this._coinIconUrl})`;
     this._coinElement.querySelector('.coin__currency-icon').style.backgroundImage = `url(images/${this._coinCurrency.toLowerCase()}.svg)`;
+    //this._coinElement.querySelector('.coin__change').textContent = this._startPrice;
     this._setUpdateInterval();
     return this._coinElement;
   }
@@ -52,6 +55,17 @@ export class Coin {
       this._coinPriceElement.style.color = 'rgb(223, 95, 103)';
     }
     this._coinPriceElement.textContent = newPrice;
+
+    this._changePercent = (100 - (this._startPrice / newPrice) * 100).toFixed(3);
+    if(this._changePercent < 0) {
+      this._coinChangeElement.style.color = 'rgb(223, 95, 103)';
+    } else if(this._changePercent > 0) {
+      this._coinChangeElement.style.color = 'rgb(5, 177, 105)';
+    } else {
+      this._coinChangeElement.style.color = 'black';
+    }
+    this._coinChangeElement.textContent = `${this._changePercent}%`;
+    
   }
 
   _setUpdateInterval() {
